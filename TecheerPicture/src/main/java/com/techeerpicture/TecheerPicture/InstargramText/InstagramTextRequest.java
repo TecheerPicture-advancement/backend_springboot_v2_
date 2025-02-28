f850a7ec8ec6703857769fcb96d7e57a2d1efde0
uropa0-0 <europa.020810@gmail.com>
2025-02-28 18:41:46 +0900

    feat: instagramText 이미지 분석 기능 추가
    기존 imageanalysis의 이미지 분석 기능을 instagramText에 병합, instagram caption를 추가적으로 가져와 문장 생성 기능 강화

--- TecheerPicture/src/main/java/com/techeerpicture/TecheerPicture/InstargramText/InstagramTextRequest.java
+++ TecheerPicture/src/main/java/com/techeerpicture/TecheerPicture/InstargramText/InstagramTextRequest.java
@@ -7,10 +7,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class InstagramTextRequest {
-
     @Schema(description = "이미지 ID", example = "1")
     private Long imageId;
 
-    @Schema(description = "생성할 광고 문장의 텍스트 프롬프트", example = "단 3일간만 할인 행사 진행하는 맥북 프로")
+    @Schema(description = "사용자가 입력한 프롬프트", example = "단 3일간만 할인 행사 진행하는 맥북 프로")
     private String textPrompt;
 }
+
